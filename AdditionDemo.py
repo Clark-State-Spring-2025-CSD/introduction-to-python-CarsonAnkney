ba9216f017ed4eeb4b6985bc4a499af84e8e5ad2
CarsonAnkney <ankneycarson@gmail.com>
2025-03-06 13:07:25 -0500

    Update AdditionDemo.py

--- AdditionDemo.py
+++ AdditionDemo.py
@@ -1,2 +1,11 @@
 #Follow the tutorial video linked in Blackboard or wait for class session, depending on enrollment
 #Video Link: https://youtu.be/N1hrsXS6dB8
+
+print("Please enter two integers to add together!")
+
+number1 = int(input("Please enter number 1: "))
+number2 = int(input("Please enter number 2: "))
+
+sum = number1 + number2
+
+print(f"The sum on {number1} and {number2} is {sum}.")
